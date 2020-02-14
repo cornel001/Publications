46770728e56140ddaa7b1ce11f1bf0d93dcb3416
cornel001 <cornel.g.ionescu@gmail.com>
2020-02-14 21:57:53 +0200

    Add project files.

---
+++ Publications/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Publications
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
