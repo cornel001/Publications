d25f19bba204c2888ddbf6b1dd61ee80c16e746c
cornel001 <cornel.g.ionescu@gmail.com>
2020-02-14 22:04:53 +0200

    Added Publication (done) and Book (under work). Program to be modified.

---
+++ Publications/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Publications
+{
+    public sealed class Book : Publication
+    {
+        public Book(string title, string author, string publisher) :
+            this(title, string.Empty, author, publisher)
+        { }
+
+        public Book(string title, string isbn, string author, string publisher) :
+            base(title, publisher, PublicationType.Book)
+        {
+            if (! string.IsNullOrEmpty(isbn))
+            {
+                if (!(isbn.Length == 10 | isbn.Length == 13))
+                    throw new ArgumentException("The ISBN must be a 10 or 13 character numeric string.");
+                ulong nISBN = 0;
+                if (! UInt64.TryParse(isbn, out nISBN))
+                    throw new ArgumentException("The ISBN can consist of numeric characters only");
+            }
+            ISBN = isbn;
+
+            Author = author;
+        }
+
+        public string ISBN { get; }
+
+        public string Author { get; }
+    }
+}
